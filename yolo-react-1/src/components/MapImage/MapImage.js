0e6646e73281e5e69050ecf9197992481f6edceb
hkim3162 <hkim3162@gmail.com>
2018-11-07 18:31:26 -0600

    TOGGLE ON for images

--- yolo-react-1/src/components/MapImage/MapImage.js
+++ yolo-react-1/src/components/MapImage/MapImage.js
@@ -1,13 +1,47 @@
+
 import React, { Component } from 'react';
+import { render } from 'react-dom'
 import PhotoAPI from '../../api/PhotoAPI';
-import Gallery from 'react-photo-gallery'
+import Lightbox from 'react-images';
+import Gallery from 'react-photo-gallery';
+
 
 
 
 class MapImage extends Component {
-  state = {
-    photos : []
+  constructor() {
+    super();
+    this.state = {
+      currentImage: 0,
+    photos: [] };
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.openLightbox = this.openLightbox.bind(this);
+    this.gotoNext = this.gotoNext.bind(this);
+    this.gotoPrevious = this.gotoPrevious.bind(this);
   }
+  openLightbox(event, obj) {
+    this.setState({
+      currentImage: obj.index,
+      lightboxIsOpen: true,
+    });
+  }
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    });
+  }
+  gotoPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+  gotoNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    });
+  }
+
 
   componentDidMount(){
     PhotoAPI.fetchPhotos()
@@ -20,13 +54,21 @@ class MapImage extends Component {
   }
 
 
+
   render() {
     console.log(this.state.photos)
     return (
       <div>
         <h1> Headshots By Han </h1>
         {/* <h2> {this.createMapImages()}  </h2> */}
-       <Gallery photos={this.state.photos} />
+       <Gallery photos={this.state.photos} onClick={this.openLightbox}/>
+       <Lightbox images={this.state.photos}
+          onClose={this.closeLightbox.bind(this)}
+          onClickPrev={this.gotoPrevious.bind(this)}
+          onClickNext={this.gotoNext.bind(this)}
+          currentImage={this.state.currentImage}
+          isOpen={this.state.lightboxIsOpen}
+        />
       </div>
         )
 
@@ -34,4 +76,6 @@ class MapImage extends Component {
 
 }
 
+
 export default MapImage
+// render(<MapImage />, document.getElementById('app'));
